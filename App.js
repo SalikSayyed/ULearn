6dbc9585de682a588e91e633bbc21bdaf5f4d91b
RutikChaudhari <64944908+Rutik20@users.noreply.github.com>
2021-09-09 19:43:06 +0530

    committed

--- App.js
+++ App.js
@@ -1,13 +1,15 @@
 import React from 'react'
 import { View, StyleSheet } from 'react-native'
 import PopularList from './components/popularCourseList'
+import Newcomponent from './components/newComponent'
 export default function App() {
   return (
-    <View style={styles.mainScreen}>
-      <View style={styles.placer}>
-        <PopularList horizontal={true} showsHorizontalScrollIndicator={false} />
-      </View>
-    </View>
+    // <View style={styles.mainScreen}>
+    //   <View style={styles.placer}>
+      <Newcomponent screenColor='blue' />
+      //  {/* <PopularList horizontal={true} showsHorizontalScrollIndicator={false} /> */}
+    //   </View>
+    // </View>
   )
 }
 
