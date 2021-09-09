4b4e6546ad42845007d5867586aa8fe9ace414a6
SalikSayyed <51844798+SalikSayyed@users.noreply.github.com>
2021-09-09 22:05:14 +0530

    linter modified, sampleScreen added

--- screens/SampleScreen.js
+++ screens/SampleScreen.js
@@ -2,17 +2,20 @@ import React from 'react'
 import { View, Text, StyleSheet } from 'react-native'
 import Colors from '../config/Colors'
 
-export default function SampleScreen() {
+export default function SampleScreen({ customStyle }) {
   return (
     <View style={styles.screenStyle}>
       <Text style={styles.textStyle}>Hello this is complete Screen and I am the TEXT on IT!!!</Text>
-      <Text style={styles.textStyle}> And This is ICON u have to use Press=> win + . "🌈"</Text>
+      <Text style={[styles.textStyle, customStyle]}>
+        {' '}
+        And This is ICON u have to Press=> win + . USE "🌈"
+      </Text>
       <Text style={styles.textStyle}>
         U Can Find This Screen From "screens" folder then "SampleScreen"
       </Text>
       <Text style={styles.textStyle}>
-        Also Every Time inorder to use any Color try to find it './config/colors' import it as
-        Colors then Colors.textRed
+        Also Every Time inorder to use any Color try to find it inside './config/colors' import it
+        as Colors then Colors.textRed
       </Text>
       <Text style={styles.textStyle}>U Should ADD YOUR OWN Colors also in ./config/colors</Text>
     </View>
